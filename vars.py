311ef6b35f12e174d2536f4938b6373b6d505479
adityass07 <ytrial677@gmail.com>
2026-02-11 10:45:39 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,8 +3,8 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "12345"))
-API_HASH = environ.get("API_HASH", "x1x2x3")
+API_ID = int(environ.get("API_ID", "30929822"))
+API_HASH = environ.get("API_HASH", "8586e9580c6480b65d23150cec959506")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 OWNER = int(environ.get("OWNER", "12345"))
@@ -23,4 +23,5 @@ if int(OWNER) not in AUTH_USERS:
 api_url = "http://master-api-v3.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
